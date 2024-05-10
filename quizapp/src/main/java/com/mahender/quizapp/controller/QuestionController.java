3319bc97011b154b216aba3ce01216f94fa0aa12
Mahender Kumar <mksb1947@gmail.com>
2024-05-10 18:04:22 +0530

    service

---
+++ quizapp/src/main/java/com/mahender/quizapp/controller/QuestionController.java
@@ -0,0 +1,23 @@
+package com.mahender.quizapp.controller;
+
+import com.mahender.quizapp.Question;
+import com.mahender.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestion(){
+        return questionService.getAllQuestions();
+    }
+}
