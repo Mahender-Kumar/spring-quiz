31418a04dac6839ac3db763ae7a6effd45bb860d
Mahender Kumar <mksb1947@gmail.com>
2024-05-13 01:16:24 +0530

    exception handling

--- quizapp/src/main/java/com/mahender/quizapp/controller/QuestionController.java
+++ quizapp/src/main/java/com/mahender/quizapp/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.mahender.quizapp.controller;
 import com.mahender.quizapp.Question;
 import com.mahender.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,20 +16,28 @@ public class QuestionController {
     QuestionService questionService;
 
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestion(){
-
+    public ResponseEntity<List<Question>> getAllQuestion(){
         return questionService.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getQuestionByCategory(@PathVariable String category ){
-
+    public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable String category ){
         return questionService.getQuestionsByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question){
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
        return questionService.addQuestion(question);
 
     }
+
+    /*@DeleteMapping("delete")
+    public String deleteQuestionById(@RequestBody Integer questionId){
+        return questionService.deleteQuestionById(questionId);
+
+    }
+    @PutMapping("update")
+    public String updateQuestionById(@RequestBody Integer questionId){
+        return questionService.updateQuestionById(questionId);
+    }*/
 }
