91ed869df20fc317ee0c7b21cf74fc682ca0db0a
Mahender Kumar <mksb1947@gmail.com>
2024-06-02 14:07:07 +0530

    done

--- quizapp/src/main/java/com/mahender/quizapp/controller/QuizController.java
+++ quizapp/src/main/java/com/mahender/quizapp/controller/QuizController.java
@@ -2,6 +2,7 @@ package com.mahender.quizapp.controller;
 
 import com.mahender.quizapp.model.Question;
 import com.mahender.quizapp.model.QuestionWrapper;
+import com.mahender.quizapp.model.Response;
 import com.mahender.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,6 +27,15 @@ public class QuizController {
     @GetMapping("get/{id}")
     public ResponseEntity<List<QuestionWrapper>> getQuiz(@PathVariable Integer id){
         return quizService.getQuizQuestions(id);
+
+    }
+
+
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id,@RequestBody List<Response> responses){
+        return quizService.calculateResult(id,responses);
+
 
     }
 }
---
+++ quizapp/src/main/java/com/mahender/quizapp/model/Response.java
@@ -0,0 +1,28 @@
+package com.mahender.quizapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Response {
+    private Integer id;
+    private String response;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
